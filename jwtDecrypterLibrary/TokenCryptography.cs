dc1b22c6e85ed2fed18e3e16954ac0e420062a81
Udit Jain <udjai@microsoft.com>
2020-05-25 22:44:57 +0530

    Add project files.

---
+++ jwtDecrypterLibrary/TokenCryptography.cs
@@ -0,0 +1,61 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Exceptions;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+
+namespace jwtDecrypterLibrary
+{
+    public class TokenCryptography
+    {
+        //const string token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjbGFpbTEiOjAsImNsYWltMiI6ImNsYWltMi12YWx1ZSJ9.8pwBI_HtXqI3UgQHQ_rDRnSQRxFL1SR8fbQoS-5kM5s";
+        //const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
+        //public void setPayload()
+        //{
+        //    var payload = new Dictionary<string, object>
+        //    {
+        //        { "claim1", 0 },
+        //        { "claim2", "claim2-value" }
+        //    };
+        //}
+        
+        public string EncodeHS256(Object payload, string secretKey)
+        {
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            var token = encoder.Encode(payload, secretKey);
+
+            return token;
+        }
+
+        public string DecodeHS256(string token, string secretKey)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                var provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+                var json = decoder.Decode(token, secretKey, verify: true);
+
+                return json;
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+            return null;
+        }
+    }
+}
---
+++ jwtTokenDecrypter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using jwtDecrypterLibrary;
+
+namespace jwtTokenDecrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TokenCryptography tc = new TokenCryptography();
+            Console.WriteLine(tc.DecodeHS256("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjbGFpbTEiOjAsImNsYWltMiI6ImNsYWltMi12YWx1ZSJ9.8pwBI_HtXqI3UgQHQ_rDRnSQRxFL1SR8fbQoS-5kM5s", "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk") ); 
+        }
+    }
+}
